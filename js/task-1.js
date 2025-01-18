fea690909e605ce4c64e881d6d9a8323331e873b
Timkaro <timkaro@i.ua>
2025-01-18 15:51:18 +0100

    create homework 1

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+// Замовлення дроїдів
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return (
+    "You ordered " + quantity + " droids worth " + totalPrice + " credits!"
+  );
+}
+console.log(makeTransaction);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+// Ширина елемента
+function getElementWidth(content, padding, border) {
+  return (
+    (Number.parseFloat(content) * 10) / 10 +
+    (Number.parseFloat(padding) * 10 * 2) / 10 +
+    (Number.parseFloat(border) * 10 * 2) / 10
+  );
+}
+console.log(getElementWidth);
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
